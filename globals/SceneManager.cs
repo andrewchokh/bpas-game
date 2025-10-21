6a8863c1f14fd58349275f3d09ceb6310c3454b9
trrt54999 <moki22810sobakacom@gmail.com>
2025-10-21 13:20:36 +0200

    Optimize scene manager

--- globals/SceneManager.cs
+++ globals/SceneManager.cs
@@ -24,71 +24,45 @@ public partial class SceneManager : Node
         Instance = this;
     }
 
-    public void Change2DScene(Node2D newScene, SceneSwap swapType)
+    public void Change2DScene(Node2D newScene, SceneSwap swapType) =>
+         ChangeScene(newScene, ref current2DScene, swapType,
+              (oldScene, newScene) => EmitSignal(SignalName.Scene2DChanged, oldScene, newScene));
+
+    public void ChangeControlScene(Control newScene, SceneSwap swapType) =>
+       ChangeScene(newScene, ref currentControlScene, swapType,
+           (oldScene, newScene) => EmitSignal(SignalName.ControlSceneChanged, oldScene, newScene));
+
+    private void ChangeScene<T>(T newScene, ref T currentScene, SceneSwap swapType, Action<T, T> onChanged)
+        where T : CanvasItem
     {
-        Node2D oldScene = current2DScene;
+        T oldScene = currentScene;
 
-        if (current2DScene != null)
+        if (currentScene == null)
+            return;
+
+        switch (swapType)
         {
-            switch (swapType)
-            {
-                case SceneSwap.DeleteScene:
-                    current2DScene.QueueFree();
-                    current2DScene = null;
-                    break;
-                case SceneSwap.HideScene:
-                    current2DScene.Hide();
-                    break;
-                case SceneSwap.RemoveScene:
-                    current2DScene.Visible = false;
-                    current2DScene.SetProcess(false);
-                    current2DScene.GetParent()?.RemoveChild(current2DScene);
-                    break;
-            }
-        }
-        if (newScene != null)
-        {
-            Globals.Instance.Root.GetRoot().AddChild(newScene);
-            Globals.Instance.Root.CurrentScene = newScene;
-            current2DScene = newScene;
-        }
-        else
-            current2DScene = null;
-
-        EmitSignal(SignalName.Scene2DChanged, oldScene, current2DScene);
-    }
-
-    public void ChangeControlScene(Control newScene, SceneSwap swapType)
-    {
-        Control oldScene = currentControlScene;
-
-        if (currentControlScene != null)
-        {
-            switch (swapType)
-            {
-                case SceneSwap.DeleteScene:
-                    currentControlScene.QueueFree();
-                    currentControlScene = null;
-                    break;
-                case SceneSwap.HideScene:
-                    currentControlScene.Hide();
-                    break;
-                case SceneSwap.RemoveScene:
-                    currentControlScene.Visible = false;
-                    currentControlScene.SetProcess(false);
-                    currentControlScene.GetParent()?.RemoveChild(currentControlScene);
-                    break;
-            }
+            case SceneSwap.DeleteScene:
+                currentScene.QueueFree();
+                break;
+            case SceneSwap.HideScene:
+                currentScene.Hide();
+                break;
+            case SceneSwap.RemoveScene:
+                Globals.Instance.Root.GetRoot().RemoveChild(currentScene);
+                break;
         }
 
-        if (newScene != null)
+        if (newScene == null)
         {
-            Globals.Instance.Root.GetRoot().AddChild(newScene);
-            currentControlScene = newScene;
+            currentScene = null;
+            onChanged?.Invoke(oldScene, currentScene);
+            return;
         }
-        else
-            currentControlScene = null;
 
-        EmitSignal(SignalName.ControlSceneChanged, oldScene, currentControlScene);
+        Globals.Instance.Root.GetRoot().AddChild(newScene);
+        currentScene = newScene;
+
+        onChanged?.Invoke(oldScene, currentScene);
     }
 }
